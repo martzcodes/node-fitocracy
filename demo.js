66ebdd485c8fe1e907b08d26a2a0d23c2bf8431a
OEHokie <matt.martz@gmail.com>
2014-10-31 19:51:39 -0400

    parsed Quests and Achievements parsed.  Probably done for now until I need something

--- demo.js
+++ demo.js
@@ -24,24 +24,34 @@ fito.getWorkout(config.username, config.password, '2012-09-20', function(workout
 fito.getWorkoutDates(config.username, config.password, '2012-09-12', '2012-09-21', function(workouts){
 	console.log('workouts',workouts);
 });
-*/
+
+fito.getUserPoints(config.username, config.password, function(points){
+	console.log('points',points);
+});
+
 fito.getUserQuests(config.username,config.password,function(quests){
 	console.log('quests',quests);
 });
-/*
+
 fito.getUserAchievements(config.username,config.password,function(achievements){
 	console.log('achievements',achievements);
 });
+*/
 
+fito.getStream(config.username, config.password, function(stream){
+	console.log('stream',stream);
+});
+
+////////////////////////////////////////////////////////
+//INCOMPLETE BELOW HERE
+////////////////////////////////////////////////////////
+
+/*
 fito.getActivityStream(config.username,config.password,function(activitystream){
 	console.log('activitystream',activitystream);
 });
 
 fito.getFeed(config.username,config.password,function(feed){
 	console.log('feed',feed);
-});
-
-fito.getStream(config.username, config.password, function(stream){
-	console.log('stream',stream);
 });
 */
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Browser = require('zombie'),
-    assert = require('assert');
+    assert = require('assert'),
+    cheerio = require('cheerio');
 
 var options = {
     waitDuration: '15s',
@@ -15,35 +16,35 @@ function footerLoaded(window) {
 }
 
 function login(username, password, callback) {
-        var browser = Browser.create(options);
-        browser.visit('https://www.fitocracy.com/accounts/login/?next=/home/', function(error) {
-            browser.on('loaded', function(loadedbrowser) {
-                console.log('loaded: ', loadedbrowser.location.href);
-            });
-            console.log(browser.errors);
-            browser.wait(footerLoaded, function() {
-                browser
-                    .fill('#username-login-username', username)
-                    .fill('#username-login-password', password)
-                    .fill('input[name="next"]', '/home/')
-                    .pressButton('#username-login-submit', function(error2) {
+    var browser = Browser.create(options);
+    browser.visit('https://www.fitocracy.com/accounts/login/?next=/home/', function(error) {
+        browser.on('loaded', function(loadedbrowser) {
+            console.log('loaded: ', loadedbrowser.location.href);
+        });
+        console.log(browser.errors);
+        browser.wait(footerLoaded, function() {
+            browser
+                .fill('#username-login-username', username)
+                .fill('#username-login-password', password)
+                .fill('input[name="next"]', '/home/')
+                .pressButton('#username-login-submit', function(error2) {
+                    browser.wait(10000, function() {
+                        titlecontent = JSON.parse(browser.text('body'));
+                        //console.log("title content:", titlecontent);
+                        //console.log('user id:', titlecontent.id);
+                        browser.location.href = 'https://www.fitocracy.com/home/';
                         browser.wait(10000, function() {
-                            titlecontent = JSON.parse(browser.text('body'));
-                            //console.log("title content:", titlecontent);
-                            //console.log('user id:', titlecontent.id);
-                            browser.location.href = 'https://www.fitocracy.com/home/';
-                            browser.wait(10000, function() {
-                                //console.log('href', browser.location.href);
-                                callback(titlecontent.id,browser);
-                            });
+                            //console.log('href', browser.location.href);
+                            callback(titlecontent.id, browser);
                         });
                     });
-            });
+                });
         });
+    });
 
-    }
+}
 exports.getActivities = function(username, password, callback) {
-    login(username,password,function(userid,browser){
+    login(username, password, function(userid, browser) {
         browser.location.href = 'https://www.fitocracy.com/get_user_activities/' + userid;
         browser.wait(10000, function() {
             var activities = JSON.parse(browser.text('body'));
@@ -54,9 +55,9 @@ exports.getActivities = function(username, password, callback) {
     });
 };
 
-exports.getActivityHistory = function(username,password,activityid,callback) {
+exports.getActivityHistory = function(username, password, activityid, callback) {
     //'https://www.fitocracy.com/_get_activity_history_json/?activity-id='+activies[X].id
-    login(username,password,function(userid,browser){
+    login(username, password, function(userid, browser) {
         browser.location.href = 'https://www.fitocracy.com/_get_activity_history_json/?activity-id=' + activityid;
         browser.wait(10000, function() {
             var activity = JSON.parse(browser.text('body'));
@@ -67,35 +68,77 @@ exports.getActivityHistory = function(username,password,activityid,callback) {
     });
 };
 
-exports.getUserQuests = function(username,password,callback) {
+exports.getUserQuests = function(username, password, callback) {
     //'https://www.fitocracy.com/get-user-quests/?user='+userid  //quests are not in json format
-    login(username,password,function(userid,browser){
+    login(username, password, function(userid, browser) {
         browser.location.href = 'https://www.fitocracy.com/get-user-quests/?user=' + userid;
         browser.wait(10000, function() {
             //these are in an html format... need to parse
-            console.log('bodytext',browser.text('body'));
-            callback(null);
-            browser.close();
+            //console.log('bodytext',browser.html('body'));
+            var data = [];
+            $ = cheerio.load(browser.html('body'));
+            var rowNums = $('tr', $('tbody', $('table'))).length;
+            console.log(rowNums);
+            for (var j = 0; j <= rowNums; j++) {
+                if (j === rowNums) {
+                    browser.close();
+                    callback(data);
+                } else {
+                    var tR = $('tr', $('tbody', $('table'))).eq(j);
+                    var rowobj = {
+                        id:parseInt(tR.attr('id').substring(tR.attr('id').indexOf('-')+1,tR.attr('id').length)),
+                        status:tR.attr('class'),
+                        name:$('.list-item-title',tR).text().replace(/\t/g, '').replace('\n', '').replace(/[\s\n\r]+/g, ' '),
+                        description:$('.description',tR).text().replace(/\t/g, '').replace('\n', '').replace(/[\s\n\r]+/g, ' '),
+                        value_points:parseInt($('.value',tR).text().replace(/\t/g, '').replace('\n', '').replace(/[\s\n\r]+/g, ''))
+                    };
+                    data.push(rowobj);
+                }
+            }
         });
     });
 };
 
-exports.getUserAchievements = function(username,password,callback) {
+exports.getUserAchievements = function(username, password, callback) {
     //'https://www.fitocracy.com/get-user-achievements/?user='+userid  //achievements are not in json format
-    login(username,password,function(userid,browser){
+    login(username, password, function(userid, browser) {
         browser.location.href = 'https://www.fitocracy.com/get-user-achievements/?user=' + userid;
         browser.wait(10000, function() {
             //these are in an html format... need to parse
-            console.log('bodytext',browser.text('body'));
-            callback(null);
-            browser.close();
+            var data = [];
+            $ = cheerio.load(browser.html('body'));
+            var rowNums = $('div').length;
+            console.log(rowNums);
+            for (var j = 0; j <= rowNums; j++) {
+                if (j === rowNums) {
+                    browser.close();
+                    callback(data);
+                } else {
+                    var isComplete = function(href) {
+                        if (href) {
+                            return "achieved";
+                        } else {
+                            return "notachieved";
+                        }
+                    };
+
+                    var tR = $('div').eq(j);
+                    var rowobj = {
+                        id:parseInt(tR.attr('id').substring(tR.attr('id').indexOf('-')+1,tR.attr('id').length)),
+                        status:isComplete($('a',tR).attr('href')),
+                        title:$('.achievement-title',tR).text().replace(/\t/g, '').replace('\n', '').replace(/[\s\n\r]+/g, ' '),
+                        description:$('.achievement-desc',tR).text().replace(/\t/g, '').replace('\n', '').replace(/[\s\n\r]+/g, ' ')
+                    };
+                    data.push(rowobj);
+                }
+            }
         });
     });
 };
 
 exports.getUserFriends = function(username, password, callback) {
     //'https://www.fitocracy.com/get-user-friends/?user='+username //friends is in json, but uses username, not id
-    login(username,password,function(userid,browser){
+    login(username, password, function(userid, browser) {
         browser.location.href = 'https://www.fitocracy.com/get-user-friends/?user=' + username;
         browser.wait(10000, function() {
             var friends = JSON.parse(browser.text('body'));
@@ -106,27 +149,14 @@ exports.getUserFriends = function(username, password, callback) {
     });
 };
 
-exports.getActivityStream = function(username,password,callback) {
-    //'https://www.fitocracy.com/activity_stream/0/?user_id='+userid+'&types=WORKOUT' //not in json //0 refers to activities?
-    login(username,password,function(userid,browser){
-        browser.location.href = 'https://www.fitocracy.com/activity_stream/0/?user_id='+userid+'&types=WORKOUT';
+exports.getUserPoints = function(username, password, callback) {
+    //https://www.fitocracy.com/get-user-points/?user=
+    login(username, password, function(userid, browser) {
+        browser.location.href = 'https://www.fitocracy.com/get-user-points/?user=' + username;
         browser.wait(10000, function() {
-            //these are in an html format... need to parse
-            console.log('bodytext',browser.text('body'));
-            callback(null);
-            browser.close();
-        });
-    });
-};
-
-exports.getFeed = function(username,password,callback) {
-    //'https://www.fitocracy.com/activity_stream/15/?user_id='+userid+'&types=WORKOUT' //not in json //15 refers to feed?
-    login(username,password,function(userid,browser){
-        browser.location.href = 'https://www.fitocracy.com/activity_stream/15/?user_id='+userid+'&types=WORKOUT';
-        browser.wait(10000, function() {
-            //these are in an html format... need to parse
-            console.log('bodytext',browser.text('body'));
-            callback(null);
+            var points = JSON.parse(browser.text('body'));
+            console.log('points body', points);
+            callback(points);
             browser.close();
         });
     });
@@ -134,8 +164,8 @@ exports.getFeed = function(username,password,callback) {
 
 exports.getWorkouts = function(username, password, callback) {
     //'https://www.fitocracy.com/api/v2/user/'+userid+'/workouts/recent/' //json
-    login(username,password,function(userid,browser){
-        browser.location.href = 'https://www.fitocracy.com/api/v2/user/'+userid+'/workouts/recent/';
+    login(username, password, function(userid, browser) {
+        browser.location.href = 'https://www.fitocracy.com/api/v2/user/' + userid + '/workouts/recent/';
         browser.wait(10000, function() {
             var workouts = JSON.parse(browser.text('body'));
             console.log('workouts body', workouts);
@@ -147,8 +177,8 @@ exports.getWorkouts = function(username, password, callback) {
 
 exports.getWorkout = function(username, password, workoutdate, callback) {
     //'https://www.fitocracy.com/api/v2/user/'+userid+'/workouts/2014-10-29/?timezone_offset=-4' //json //workouts from specific day in YYYY-MM-DD format
-    login(username,password,function(userid,browser){
-        browser.location.href = 'https://www.fitocracy.com/api/v2/user/'+userid+'/workouts/'+workoutdate+'/?timezone_offset=-4';
+    login(username, password, function(userid, browser) {
+        browser.location.href = 'https://www.fitocracy.com/api/v2/user/' + userid + '/workouts/' + workoutdate + '/?timezone_offset=-4';
         browser.wait(10000, function() {
             var workouts = JSON.parse(browser.text('body'));
             console.log('workouts body', workouts);
@@ -160,8 +190,8 @@ exports.getWorkout = function(username, password, workoutdate, callback) {
 
 exports.getWorkoutDates = function(username, password, startdate, enddate, callback) {
     //'https://www.fitocracy.com/api/v2/user/555068/workouts/?start_date=2014-09-28&end_date=2014-11-01&ids_and_names_only=1&timezone_offset=-4'
-    login(username,password,function(userid,browser){
-        browser.location.href = 'https://www.fitocracy.com/api/v2/user/555068/workouts/?start_date='+startdate+'&end_date='+enddate+'&ids_and_names_only=1&timezone_offset=-4';
+    login(username, password, function(userid, browser) {
+        browser.location.href = 'https://www.fitocracy.com/api/v2/user/555068/workouts/?start_date=' + startdate + '&end_date=' + enddate + '&ids_and_names_only=1&timezone_offset=-4';
         browser.wait(10000, function() {
             var workouts = JSON.parse(browser.text('body'));
             console.log('workouts body', workouts);
@@ -173,12 +203,39 @@ exports.getWorkoutDates = function(username, password, startdate, enddate, callb
 
 exports.getStream = function(username, password, callback) {
     //'https://www.fitocracy.com/stream_facepile/'  //stream, json
-    login(username,password,function(userid,browser){
+    login(username, password, function(userid, browser) {
         browser.location.href = 'https://www.fitocracy.com/stream_facepile/';
         browser.wait(10000, function() {
             var stream = JSON.parse(browser.text('body'));
             console.log('stream body', stream);
             callback(stream);
+            browser.close();
+        });
+    });
+};
+
+exports.getActivityStream = function(username, password, callback) {
+    //'https://www.fitocracy.com/activity_stream/0/?user_id='+userid+'&types=WORKOUT' //not in json //0 refers to item count (0 is 0:14, 15 is 15:29, etc)
+    login(username, password, function(userid, browser) {
+        browser.location.href = 'https://www.fitocracy.com/activity_stream/0/?user_id=' + userid + '&types=WORKOUT';
+        browser.wait(10000, function() {
+            //these are in an html format... need to parse
+            var data = [];
+            $ = cheerio.load(browser.html('.stream-inner'));
+            var rowNums = $('.stream_item').length;
+            console.log(rowNums);
+        });
+    });
+};
+
+exports.getFeed = function(username, password, callback) {
+    //'https://www.fitocracy.com/activity_stream/15/?user_id='+userid //not in json
+    login(username, password, function(userid, browser) {
+        browser.location.href = 'https://www.fitocracy.com/activity_stream/0/?user_id=' + userid;
+        browser.wait(10000, function() {
+            //these are in an html format... need to parse
+            console.log('bodytext', browser.text('body'));
+            callback(null);
             browser.close();
         });
     });
